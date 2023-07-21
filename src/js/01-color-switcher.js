5fb29544d893ac7582db0f18c91a3cd3879cf0a7
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2023-07-21 11:32:17 +0300

    add

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,14 @@
+const btnStartEl = document.querySelector('.button[data-start]');
+const btnStopEl = document.querySelector('.button[data-stop]');
 
+btnStartEl.addEventListener('click', onStart);
+btnStopEl.addEventListener('click', onStop);
+
+function onStart() {}
+function onStop() {}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
