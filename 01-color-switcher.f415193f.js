30a13fd3d14affd78354d839ad90e4e8b9c722b9
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2023-07-21 10:08:29 +0000

    Deploying to gh-pages from @ Natalka333/goit-js-hw-09@b575be63cde6204978f64c89ee09d5a134a92991 🚀

---
+++ 01-color-switcher.f415193f.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null;t.addEventListener("click",(function(){n=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));document.body.style.backgroundColor=t}),1e3),t.toggleAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(n),t.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.f415193f.js.map
